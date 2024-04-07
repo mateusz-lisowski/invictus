f3b21ce6a9e31697d6cdb3311a8e76cd23370dd4
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-04-07 18:54:45 +0200

    Create working data feed topic

---
+++ server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
+
+type server struct {
+	connections map[*websocket.Conn]bool
+}
+
+func newServer() *server {
+	return &server{
+		connections: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *server) handleBoard(conn *websocket.Conn) {
+	fmt.Println("New incoming connection to board handler from client: ", conn.RemoteAddr())
+	for {
+		payload := fmt.Sprint("board state: ", time.Now())
+		conn.Write([]byte(payload))
+		time.Sleep(time.Second * 3)
+	}
+}
+
+func main() {
+	const port int = 8080
+
+	server := newServer()
+
+	http.Handle("/board", websocket.Handler(server.handleBoard))
+
+	fmt.Printf("Listening on port %d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
