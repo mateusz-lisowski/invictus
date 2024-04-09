c398e19d6be933c0a408218e0b79aa337504889b
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-04-09 20:08:24 +0200

    Move test chat implementation to dedicated directory

---
+++ server-test/go.mod
@@ -0,0 +1,5 @@
+module github.com/mateusz-lisowski/invictus
+
+go 1.22.0
+
+require golang.org/x/net v0.24.0 // indirect
---
+++ server-test/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
+
+type server struct {
+	connections map[*websocket.Conn]bool
+}
+
+func newServer() *server {
+	return &server{
+		connections: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *server) handleBoard(conn *websocket.Conn) {
+	fmt.Println("New incoming connection to board handler from client: ", conn.RemoteAddr())
+	for {
+		payload := fmt.Sprint("board state: ", time.Now())
+		conn.Write([]byte(payload))
+		time.Sleep(time.Second * 3)
+	}
+}
+
+type game struct {
+  board []string
+  possibleColors []string
+  usedColors []string
+} 
+
+func newGame(boardSize int, possibleColors []string) *game {
+  return &game{
+    board: make([]string, boardSize),
+    possibleColors: possibleColors,
+    usedColors: make([]string, len(possibleColors)),
+  }
+}
+
+func (g *game) mainLoop(s *server) {
+  go func() {
+    for {
+      fmt.Println("xD")
+      time.Sleep(time.Second * 2)
+    }
+  }() 
+}
+
+
+func main() {
+	const port int = 8080
+  const size int = 64
+  possibleColors := []string{"red", "green", "blue"}
+
+	server := newServer()
+  game := newGame(size, possibleColors)
+  game.mainLoop(server)
+
+	http.Handle("/board", websocket.Handler(server.handleBoard))
+
+	fmt.Printf("Listening on port %d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
