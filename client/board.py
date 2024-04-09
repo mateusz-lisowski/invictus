b2c4083f7eb74ecf7a5f96468012ca08da5dba4b
Wojciech Słomowicz <wojtek.slomowicz@gmail.com>
2024-04-09 12:58:29 +0200

    Add drawing of board in client

---
+++ client/board.py
@@ -0,0 +1,84 @@
+from draw import *
+from player import *
+
+class BoardScreen(Viewer):
+	def __init__(self, owner, player):
+		self.player = player
+		super().__init__(owner, self.player.board_height + 2, 2 * self.player.board_width + 4)
+		self.__colors = {
+			"BLACK": curses.color_pair(0),
+			"BLUE": curses.color_pair(1),
+			"GREEN": curses.color_pair(2),
+			"YELLOW": curses.color_pair(6),
+		}
+		curses.halfdelay(1)
+		self.cursor_x = 0
+		self.cursor_y = 0
+		self.cursor_blink = 0
+
+	def draw(self):
+		self.__updateSize()
+		self.scr.clear()
+
+		drawBorder(self.scr, 0, 0, self.height, self.width // 2, curses.color_pair(0))
+		self.__draw_map()
+		self.__draw_cursor()
+
+	def __draw_map(self):
+		for y in range(self.player.board_height):
+			for x in range(self.player.board_width):
+				cell = self.player.board[y * self.player.board_width + x]
+				if cell != "BLACK":
+					color = self.__colors[cell]
+					self.scr.addstr(y + 1, 2 * x + 2, "██", color)
+
+	def __draw_cursor(self):
+		cursor_blink = (self.cursor_blink // 4) % 2
+		if cursor_blink == 1:
+			return
+		self.scr.addstr(self.cursor_y + 1, 2 * self.cursor_x + 2, "██")
+
+
+	def handleInput(self, ch):
+		move_x = 0
+		move_y = 0
+
+		if ch == ord('q'):
+			self.owner.content = None
+			self.owner.closed = True
+		elif ch == curses.KEY_UP:
+			move_y = -1
+		elif ch == curses.KEY_DOWN:
+			move_y = +1
+		elif ch == curses.KEY_LEFT:
+			move_x = -1
+		elif ch == curses.KEY_RIGHT:
+			move_x = +1
+		if ch == -1:
+			self.cursor_blink += 1
+		
+		if move_x != 0 or move_y != 0:
+			self.__moveCursor(self.cursor_y + move_y, self.cursor_x + move_x)
+			self.cursor_blink = 0
+
+
+	def __moveCursor(self, y, x):
+		self.cursor_x = max(0, min(x, self.player.board_width - 1))
+		self.cursor_y = max(0, min(y, self.player.board_height - 1))
+		self.offset_x = max(2 * self.cursor_x - self.owner.width + 9, min(self.offset_x, 2 * self.cursor_x + 2))
+		self.offset_y = max(self.cursor_y - self.owner.height + 4, min(self.offset_y, self.cursor_y + 1))
+
+		if self.width > self.owner.width - 2:
+			self.offset_x = 2 * max(1, min(self.offset_x // 2, (self.width - self.owner.width + 3) // 2))
+		else:
+			self.offset_x = 0
+
+		if self.height > self.owner.height - 2:
+			self.offset_y = max(1, min(self.offset_y, self.height - self.owner.height + 1))
+		else:
+			self.offset_y = 0
+
+	def __updateSize(self):
+		if self.height != self.player.board_height or self.width != self.player.board_width:
+			self.resize(self.player.board_height + 2, 2 * self.player.board_width + 4)
+			self.__moveCursor(self.cursor_y, self.cursor_x)
---
+++ client/player.py
@@ -0,0 +1,42 @@
+import json
+
+class Player:
+    def __init__(self, color):
+        self.__login(color)
+        self.scores = None
+
+    def logout(self):
+        response = self.__sendRequest("", {"uuid": self.uuid})
+        return response['success']
+
+    def sendMove(self, moves):
+        response = self.__sendRequest("", {"board": moves, "uuid": self.uuid})
+        return response['success']
+
+    def updateScores(self):
+        response = self.__sendRequest("", {"uuid": self.uuid})
+        self.scores = response['scores']
+
+    def __login(self, color):
+        response = self.__sendRequest("", {"color": color})
+        self.uuid = response['uuid']
+        self.color = response['color']
+        self.__updateBoard()
+        self.__updateCells()
+
+    def __updateBoard(self):
+        response = self.__sendRequest("", {"uuid": self.uuid})
+        self.board = response['board']
+        self.board_width = response['width']
+        self.board_height = response['height']
+
+    def __updateCells(self):
+        response = self.__sendRequest("", {"uuid": self.uuid})
+        self.cells = response['cells']
+
+    def __sendRequest(self, url, data):
+        #x = requests.post(url, json=data)
+        file = open("data.json")
+        x = json.load(file)
+        return x
+
